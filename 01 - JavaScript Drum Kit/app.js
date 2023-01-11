8dc8456c78f22eade44694cdf9d3a2a282ff1cdf
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-01-11 22:53:58 +0100

    feat(drum-kit): add style changes on playing audio

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -2,6 +2,7 @@ window.addEventListener("keydown", (e) => {
   // keyCode is deprecated, but the starter files were created
   // based on keyCode, so I'm keeping it here.
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) {
     return;
   }
@@ -9,4 +10,15 @@ window.addEventListener("keydown", (e) => {
   // Always start playing the audio from the beginning.
   audio.currentTime = 0;
   audio.play();
+  key.classList.add("playing");
 });
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) =>
+  key.addEventListener("transitionend", (e) => {
+    if (e.propertyName !== "transform") {
+      return;
+    }
+    key.classList.remove("playing");
+  })
+);
