c25f0aa6560317001524427b12cb34343eb36f3d
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-01-14 12:46:36 +0100

    feat(canvas): make line color gradient

--- 08 - Fun with HTML5 Canvas/app.js
+++ 08 - Fun with HTML5 Canvas/app.js
@@ -3,19 +3,20 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 function draw(e) {
   if (!isDrawing) {
     return;
   }
 
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   // Begin line here.
   ctx.moveTo(lastX, lastY);
@@ -25,6 +26,7 @@ function draw(e) {
 
   lastX = e.offsetX;
   lastY = e.offsetY;
+  hue++;
 }
 
 canvas.addEventListener("mousedown", (e) => {
