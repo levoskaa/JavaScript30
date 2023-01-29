2cc84f104c8623003376725c23a8e7f3c1f6956c
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-01-29 20:47:15 +0100

    feat(storage): implement basic task

--- 15 - LocalStorage/app.js
+++ 15 - LocalStorage/app.js
@@ -1,3 +1,45 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
-const items = [];
+const items = JSON.parse(localStorage.getItem("items")) || [];
+
+function addItem(e) {
+  // Prevent page from reloading on submit.
+  e.preventDefault();
+  const text = this.querySelector("[name=item]").value;
+  const item = {
+    text,
+    done: false,
+  };
+  items.push(item);
+  displayList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
+  this.reset();
+}
+
+function toggleDone(e) {
+  if (!e.target.matches("input")) {
+    return;
+  }
+  const index = e.target.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+function displayList(items = [], destination) {
+  destination.innerHTML = items
+    .map((item, i) => {
+      return `
+        <li>
+          <input type="checkbox" id="item${i}" data-index="${i}" ${
+        item.done ? "checked" : ""
+      }/>
+          <label for="item${i}">${item.text}</label>
+        </li>`;
+    })
+    .join("");
+}
+
+addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
+
+displayList(items, itemsList);
